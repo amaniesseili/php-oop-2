f9e48da62eca353ed9e18c20c1b723bd8f62d4c6
amani esseili <esseiliamani@hotmail.com>
2023-09-20 16:42:51 +0200

    prodotto.php

---
+++ prodotto.php
@@ -0,0 +1,22 @@
+<?php
+
+// Classe per rappresentare un Prodotto
+class Prodotto {
+    private $nome;
+    private $prezzo;
+    private $immagine;
+
+    // Costruttore per inizializzare le proprietà
+    public function __construct($nome, $prezzo, $immagine,) {
+        $this->nome = $nome;
+        $this->prezzo = $prezzo;
+        $this->immagine = $immagine;
+    }
+
+    // Getter per ottenere il nome del Prodotto
+    public function getNome() {
+        return $this->nome;
+    }
+
+
+}
